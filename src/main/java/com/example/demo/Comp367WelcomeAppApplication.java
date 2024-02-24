ea45f536dd4a254c634c1d58420eddb5ccc0d810
Parth Sharma <pshar251@my.centennialcollege.ca>
2024-02-24 16:17:56 -0500

    Initial commit

---
+++ src/main/java/com/example/demo/Comp367WelcomeAppApplication.java
@@ -0,0 +1,13 @@
+package com.example.demo;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class Comp367WelcomeAppApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(Comp367WelcomeAppApplication.class, args);
+	}
+
+}
